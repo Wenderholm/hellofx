bd0b9d9bd8134e30233208c0890f357cdd29b5c6
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-09-21 22:50:00 +0200

    style CSS w fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,10 +9,12 @@ module hellofx {
     exports pl.javastart.fxactionExe.main to javafx.graphics;
     exports pl.javastart.fxmodules.main to javafx.graphics;
     exports pl.javastart.fxproperties.main to javafx.graphics;
+    exports pl.javastart.cssnotepad.main to javafx.graphics;
 
     opens pl.javastart.fxctrl.controller to javafx.fxml;
     opens pl.javastart.fxaction.controller to javafx.fxml;
     opens pl.javastart.fxactionExe.controller to javafx.fxml;
     opens pl.javastart.fxmodules.controller to javafx.fxml;
     opens pl.javastart.fxproperties.controller to javafx.fxml;
+    opens pl.javastart.cssnotepad.controller to javafx.fxml;
 }
